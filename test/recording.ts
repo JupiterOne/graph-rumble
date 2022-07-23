a6b3de3293892e2dd735c7c8786f6d62be90dbf3
Matthew Zember <matthew.zember@jupiterone.com>
2022-07-23 15:03:38 -0400

    upgrade: improve redaction workflow

--- test/recording.ts
+++ test/recording.ts
@@ -30,40 +30,28 @@ export function setupRumbleRecording(
   });
 }
 
-function redactFromJSONL(entry): void {
+function applyPreredactionMutations(entry) {
   mutations.unzipGzippedRecordingEntry(entry);
-  const DEFAULT_REDACT = '[REDACTED]';
-  const keysToRedactMap = new Map();
-  keysToRedactMap.set('export_token', DEFAULT_REDACT);
-  keysToRedactMap.set('download_token', DEFAULT_REDACT);
-  keysToRedactMap.set('export_token_last_used_by', DEFAULT_REDACT);
-  keysToRedactMap.set('first_name', DEFAULT_REDACT);
-  keysToRedactMap.set('last_name', DEFAULT_REDACT);
-  // email needs to fit the email format
-  keysToRedactMap.set('email', 'first.last@example.com');
-  keysToRedactMap.set('last_login_ip', DEFAULT_REDACT);
-  keysToRedactMap.set('last_task_by', 'first.last@example.com');
+}
+
+function redactFromJSONL(entry): void {
   const jsonItems = entry.response.content.text.split('\n');
   let redactedResponse = '';
   for (const jsonItem of jsonItems) {
     const jsonObject = JSON.parse(jsonItem);
-
-    if (jsonObject.forEach) {
-      jsonObject.forEach((jsonValue, jsonIndex) => {
-        keysToRedactMap.forEach((redactionValue, keyToRedact) => {
-          if (jsonValue[keyToRedact]) {
-            jsonObject[jsonIndex][keyToRedact] = redactionValue;
-          }
-        });
-      });
+    for (const [responseKey, responseValue] of jsonObject.entries()) {
+      for (const [redactionKey, redactionValue] of redactionMap.entries()) {
+        if (responseValue[redactionKey]) {
+          jsonObject[responseKey][redactionKey] = redactionValue;
+        }
+      }
     }
     redactedResponse += JSON.stringify(jsonObject) + '\n';
   }
   entry.response.content.text = redactedResponse;
 }
 
-function redact(entry): void {
-  mutations.unzipGzippedRecordingEntry(entry);
+function getRedactionMap(): Map<string, any> {
   const DEFAULT_REDACT = '[REDACTED]';
   const keysToRedactMap = new Map();
   keysToRedactMap.set('export_token', DEFAULT_REDACT);
@@ -75,20 +63,34 @@ function redact(entry): void {
   keysToRedactMap.set('email', 'first.last@example.com');
   keysToRedactMap.set('last_login_ip', DEFAULT_REDACT);
   keysToRedactMap.set('last_task_by', 'first.last@example.com');
-  const response = JSON.parse(entry.response.content.text);
+  return keysToRedactMap;
+}
 
-  if (response.forEach) {
-    response.forEach((responseValue, responseIndex) => {
-      keysToRedactMap.forEach((redactionValue, keyToRedact) => {
-        if (responseValue[keyToRedact]) {
-          response[responseIndex][keyToRedact] = redactionValue;
+const redactionMap = getRedactionMap();
+
+function redact(entry): void {
+  applyPreredactionMutations(entry);
+
+  try {
+    const response = JSON.parse(entry.response.content.text);
+
+    if (Array.isArray(response)) {
+      for (const [responseIndex, responseValue] of response.entries()) {
+        for (const [redactionKey, redactionValue] of redactionMap.entries()) {
+          if (responseValue[redactionKey]) {
+            response[responseIndex][redactionKey] = redactionValue;
+          }
         }
-      });
-      if ('services' in responseValue) {
-        redactFromServices(responseValue.services);
+
+        if ('services' in responseValue) {
+          redactFromServices(responseValue.services);
+        }
       }
-    });
-    entry.response.content.text = JSON.stringify(response);
+      entry.response.content.text = JSON.stringify(response);
+    }
+  } catch (err) {
+    // try to redact as JSONL
+    redactFromJSONL(entry);
   }
 }
 
